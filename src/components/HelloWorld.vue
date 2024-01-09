4e85f1a0bfe1b33c8a25ecf0e25fc2884047ae45
Ningsang Jabegu <86409265+Ningsang-Jabegu@users.noreply.github.com>
2024-01-09 11:23:42 +0545

    Initial commit to Mr. Kshitiz

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <img src="/assets/images/Profile Picture.jpg">
+  <h1>{{ msg }}</h1>
+  <h2>Jado ma bug lai liyera ghar pharkhandai. 😂😂😂 Comming Soon !</h2>
+</template>
+
+<style scoped>
+
+</style>
